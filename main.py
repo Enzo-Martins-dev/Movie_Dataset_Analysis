11edc5d2f57e571b7831b52bfa8c5d95ec62e269
Enzo Martins <enzomartinsdev5@gmail.com>
2024-09-30 16:09:47 -0300

    Tratando Earnings e Adicionando Gráfico

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def categoria_decada():
 
 categoria_decada()
 
-def nota_faturamento():
+def nota_faturamento_dist():
     '''Plotar Gráfico que mostre a relação entre nota IMDb do filme e seu faturamento.
     Aceitar filtro para mostrar apenas elementos com nota e/ou faturamento entre x e y.
 
@@ -43,11 +43,12 @@ def nota_faturamento():
     faturamento_min = st.text_input('Digite o faturamento mínimo', 0, len(df['Earnings'].max()))
     faturamento_max = st.text_input('Digite o faturamento máximo', 0, len(df['Earnings'].max()))
     '''
+    st.write('Gráfico de relação entre nota IMDb e faturamento')
 
-    nota_min = float(st.text_input('Digite a nota mínima', 0, 4))
-    nota_max = float(st.text_input('Digite a nota máxima', 0, 4))
-    faturamento_min = float(st.text_input('Digite o faturamento mínimo', 0, 12))
-    faturamento_max = float(st.text_input('Digite o faturamento máximo', 0, 12))
+    nota_min = float(st.text_input('Digite a nota mínima', 0, 4, key='nota_min_dist'))
+    nota_max = float(st.text_input('Digite a nota máxima', 0, 4, key='nota_max_dist'))
+    faturamento_min = float(st.text_input('Digite o faturamento mínimo', 0, 12, key='faturamento_min_dist'))
+    faturamento_max = float(st.text_input('Digite o faturamento máximo', 0, 12, key='faturamento_max_dist'))
 
     df_filtrado = df[(df['IMDb score'] >= nota_min) & (df['IMDb score'] <= nota_max)
                      & (df['Earnings'] >= faturamento_min) & (df['Earnings'] <= faturamento_max)]
@@ -60,8 +61,42 @@ def nota_faturamento():
         y = 'Earnings',
     )
 
-    grafico_nota_faturamento.set(xlabel='Nota IMDb', ylabel='Earnings (Em Milhões)')
+    grafico_nota_faturamento.set(xlabel='Nota IMDb', ylabel='Faturamento')
     
     st.pyplot(plt)
 
-nota_faturamento()
+nota_faturamento_dist()
+
+
+def nota_faturamento_media():
+    st.write('Gráfico com média de faturamento por grupo de nota IMDb')
+
+    nota_min = float(st.text_input('Digite a nota mínima', 0, 4, key='nota_min_media'))
+    nota_max = float(st.text_input('Digite a nota máxima', 0, 4, key='nota_max_media'))
+    faturamento_min = float(st.text_input('Digite o faturamento mínimo', 0, 12, key='faturamento_min_media'))
+    faturamento_max = float(st.text_input('Digite o faturamento máximo', 0, 12, key='faturamento_max_media'))
+
+    df_filtrado = df[(df['IMDb score'] >= nota_min) & (df['IMDb score'] <= nota_max)
+                     & (df['Earnings'] >= faturamento_min) & (df['Earnings'] <= faturamento_max)]
+    
+    bins = [i for i in range(0, 11)]
+    labels = [f'{i}-{i+1}' for i in range(0,10)] 
+
+    df_filtrado['IMDb group'] = pd.cut(df_filtrado['IMDb score'], bins = bins, labels = labels, include_lowest = True)
+    
+    df_filtrado_agrupado_media = df_filtrado.groupby('IMDb group')['Earnings'].mean().reset_index()
+
+
+    grafico_media_nota_faturamento = sns.barplot(
+        data = df_filtrado_agrupado_media,
+        x = 'IMDb group',
+        y = 'Earnings'
+    )
+
+    grafico_media_nota_faturamento.set(xlabel = 'Nota IMDb', ylabel = 'Faturamento Médio')
+
+    st.pyplot(plt)
+    
+
+
+nota_faturamento_media()
